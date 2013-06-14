ddaf53146a1f82a04d4e8065d321513ff02b54ab
Ubuntu <ubuntu@chex.dyndns.biz>
2013-06-14 06:57:45 +0000

    tweaked setup

--- setup.sh
+++ setup.sh
@@ -1,28 +1,47 @@
 #!/usr/bin/env bash
+clear
+function ClearWay () {
+	echo "Permanently deleting $1...";
+	rm -rf $1
+}
 
+function LinkIt () {
+	ClearWay $2;
+	
+	echo "Linking [ $1 ] ==> [ $2 ]";
+	ln -s $1 $2;
+} 
+function Title () {
+	echo -e  "\r\n=========\r\n$1\r\n"
+}
+function ReMake () {
+	echo "Re-creating $1";
+	rm -rf $1;
+	mkdir $1;
+}
+
+
+# common dirs
 HOME=~
 DF=$HOME/dotfiles
 BAK=/tmp/script_backups
 
-echo "CD [$HOME]"
-cd $HOME
-
-echo "BAK [$BAK]"
-
-echo "Re-creating backup dir."
-rm -rf $BAK;
-mkdir $BAK
-
 # set vim vars
-VIMDIR=$HOME/.vim/
+VIMDIR=$HOME/.vim
 VIMRC=$HOME/.vimrc
 
-# bak vim confs
-echo "vim dir [$VIMDIR] conf [$VIMRC]"
-rm -rf $VIMDIR
-rm -rf $VIMRC
+# =====
+# INTRO
+# =====
+echo "home  --  $HOME | vimd  --  $VIMDIR"
+echo "dotf  --  $DF | virc  --  $VIMRC"
+echo -e "back  --  $BAK \r\n====================\r\n"
 
-echo "Symlinking the vim bullshits"
-ln -s $DF/vim $VIMDIR
-ln -s $DF/vim/config $VIMRC 
+# VIM cfgs
+Title "Symlinking the vim bullshits"
+LinkIt $DF/vim/config $VIMRC 
+LinkIt $DF/vim $VIMDIR
 
+Title "Linking pathogen.vim..."
+p="pathogen"
+LinkIt $VIMDIR/bundle/$p/autoload/$p.vim $VIMDIR/autoload/$p.vim
