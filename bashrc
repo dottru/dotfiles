bfae4db2a819adef0b44571d68a2d942eef3c636
Joshua <dottru@gmail.com>
2013-06-17 17:16:10 +0000

    various changes

---
+++ bashrc
@@ -0,0 +1,8 @@
+alias apt-get="sudo apt-get -y "
+alias aptitude="sudo aptitude -y "
+alias update="aptitude update "
+alias install="aptitude install "
+
+gitacp() { git add $1; git commit -m "$2"; git push; }
+
+
--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 clear
+
+# Func defs
 function ClearWay () {
 	echo "Permanently deleting $1...";
 	rm -rf $1
 }
-
 function LinkIt () {
 	ClearWay $2;
 	
@@ -31,13 +32,13 @@ VIMDIR=$HOME/.vim
 VIMRC=$HOME/.vimrc
 
 # =====
-# INTRO
+# INTRO VARS
 # =====
 echo "home  --  $HOME | vimd  --  $VIMDIR"
 echo "dotf  --  $DF | virc  --  $VIMRC"
 echo -e "back  --  $BAK \r\n====================\r\n"
 
-# VIM cfgs
+# VIM
 Title "Symlinking the vim bullshits"
 LinkIt $DF/vim/config $VIMRC 
 LinkIt $DF/vim $VIMDIR
@@ -47,9 +48,20 @@ p="pathogen"
 mkdir $VIMDIR/autoload
 LinkIt $VIMDIR/bundle/$p/autoload/$p.vim $VIMDIR/autoload/$p.vim
 
-
+# Git
 Title "Linking gitconfig"
 LinkIt $DF/gitconfig $HOME/.gitconfig
 
+# SSH
+Title "SSH keys"
+PUBKEY=$HOME/.ssh/id_rsa.pub
+DEST=$DF/ssh_keys/`whoami`_`hostname`.pub
 
+if [ ! -f $PUBKEY ]; then
+	ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ""
+else
+	echo "No need to generate a key..."
+fi
 
+cat ~/.ssh/id_rsa.pub >> $DEST 
+git add $DEST && git commit -m "Generated pubkey for `whoami`@`hostame`" && git push
