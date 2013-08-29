17a4243dfa568f67a28ecdf4a94714f784542d6c
Joshua Mikkael <dottru@gmail.com>
2013-08-29 20:15:35 +0000

    git love

--- init/git.sh
+++ init/git.sh
@@ -21,27 +21,26 @@ ln -s `pwd`/link/gitconfig $GCFG;
 #inp " Enter your git name} " && git config --global user.name "$REPLY"
 #inp "Enter your git email} " && git config --global user.email "$REPLY"
 
-set -e 
 function AutoPush () {
   # Make it push on commit
   HOOK=".git/hooks/post-commit";
-  lhook="copy/post-commit";
+  cp copy/post-commit /tmp;
+  lhook="/tmp/post-commit";
   
   if [ ! -z $1 ]; then
     GO="$1";
-    Msg "Moved to '$1'.";
-    cp $lhook /tmp;
-    lhook="/tmp/post-commit";
+
+    Msg "|| Moved to '$1'.";
     pushd "$GO";
   fi;
 
-  echo "[Configuring git to auto-push to master on commit.]";
+  Msg "[Configuring git to auto-push to master on commit.]";
   cp $lhook $HOOK;
-  chmod +x $HOOK
-  git init
+  chmod +x $HOOK;
+  git init;
   [ ! -z $GO ] && popd;
 
-  echo "Git configuration completed..";
+  Msg "Git configuration completed..";
 }
 
 echo `AutoPush .`;
