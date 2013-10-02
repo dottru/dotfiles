582f53bcf59a6ac1aab5df22485b435ae56be7ff
Joshua Mikkael <dottru@gmail.com>
2013-10-02 12:07:07 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-clear; 
-
 . lib/control.sh        # control flow
 . lib/globals.sh        # global vars
 . lib/txt.sh            # text formatting
-
-  # Fixes local git uri so password gets cached
 . lib/git.sh # will set the remote uri for `pwd`
 
-
 #######################################
+
+    BRANCH=`git rev-parse --abbrev-ref HEAD`;
 
     Section " jmkogut's dotfiles repository ";
-    Msg " using branch // $(GetBranch) //";
-
-#######################################
-exit 1; 
-
+    Msg " pwd    --`pwd`";
+    Msg " branch -- ${BRANCH}";
+    Pause;
 
 #######################################
 Msg "Updating git submodules.";
@@ -25,51 +20,42 @@ git submodule update --init --recursive
 Pause;
 #######################################
 
-
+### Pkg Manager
 function PkgMan () {
     Msg "Running following script as root.";
     sudo bash ./init/pkgman.sh;
+    sudo bash ./init/packages.sh # Installs various dev packages
 }
 Confirmation "Run package manager discovery? " PkgMan;
-Pause;
+###
+
 
 # Setup vim
 . vim/setup.sh
-
-Pause;
-
-sudo bash init/packages.sh # Installs various dev packages
-
 Pause;
 
 # Links dotfiles to $HOME. Unused atm.
 . init/linker.sh 
-
 Pause;
-
 
 # Set hostname
 . init/hostname.sh
-
 Pause;
 
 
 # Create new user account
 . init/newuser.sh
-
 Pause;
 
 
 # Decrypt secrets
 . init/secrets.sh
-
 Pause;
 
 
 # S3 credentials
 . init/s3-creds.sh
 . init/buckets.sh
-
 Pause;
 
 
